df64f923638da585bc5f5eb9102d368ace121eb1
pikachua7 <atharvapaliwal7@gmail.com>
2023-07-23 23:44:03 +0530

    connect mongoose

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+import mongoose from 'mongoose';
+
 const app = express();
 
 app.use(cors({
@@ -20,3 +22,10 @@ const server = http.createServer(app);
 server.listen(8080, ()=>{
     console.log('Server running on http://localhost:8000/');
 })
+
+
+const MONGO_URL = "mongodb+srv://a7ath:i0goIFJXfXiivWaD@cluster0.c83bjyc.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error',(error:Error)=>console.log(error));
